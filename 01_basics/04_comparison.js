e84c122c6aca0a1b82a8b40abb1adb486515f23c
tyagiashutosh629 <110218686+tyagiashutosh629@users.noreply.github.com>
2024-07-04 23:12:10 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,7 +8,7 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
+console.log(null > 0);     //Important note-  check in notebook(reason)
 console.log(null == 0);
 console.log(null >= 0);
 
